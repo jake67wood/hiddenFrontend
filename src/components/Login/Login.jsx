304a8bc13d647412339503d79222fa08898e690d
jake67wood <jakewood6788@gmail.com>
2025-03-14 10:07:06 -0600

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login({ setUser }) {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await axios.post('http://localhost:5000/api/auth/login', { username, password }, { withCredentials: true });
+			const res = await axios.post('https://hiddenserver-i7sc.onrender.com/api/auth/login', { username, password }, { withCredentials: true });
 
 			if (res.status === 200) {
 				console.log(res.data.user);
